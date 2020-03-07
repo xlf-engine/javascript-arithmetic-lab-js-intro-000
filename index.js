440b4807ecbad9fbe872471b173777f6baffb6d2
NT <nicolasturek@gmail.com>
2020-03-07 01:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function makeInt(string) {
-  return parseInt(`${string}`, 10);
+
+
+function makeInt(n) {
+  return parseInt(`${n}`, 10);
 }
 
-function preserveDecimal(string) {
-  return parseFloat(`${string}`);
+function preserveDecimal(n) {
+  return parseFloat(`${n}`);
 }
