5dd906bb46092d93c911d097f5d921c5ba001cec
NT <nicolasturek@gmail.com>
2020-03-07 02:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+function add(a,b) {
+  return a + b;
+}
 
+function subtract(a,b){
+  return a - b;
+
+}
+
+function multiple(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+
+}
+
+function increment(n){
+  return n++;
+}
+
+function decrement(n){
+  return n--
+}
 
 function makeInt(n) {
   return parseInt(`${n}`, 10);
